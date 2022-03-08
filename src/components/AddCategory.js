47f6d54386c886be9f11ff1bb00ef020ea5a3c26
Alexander <47396008+AlexanderCarreno@users.noreply.github.com>
2022-03-07 19:53:02 -0600

    AddCategory component created

---
+++ src/components/AddCategory.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ({
+  setCategories
+}) => {
+
+const [inputValue, setInputValue] = useState('');
+
+const handleInputChange = (e)=> {
+    console.log(e.target.value);
+    setInputValue( e.target.value );
+}
+
+const handleSubtim = (e) => {
+    e.preventDefault();
+
+    if( inputValue.trim().length > 2 ){
+      setCategories( cats => [...cats, inputValue]);
+      setInputValue('');
+      //console.log('Submit hecho');
+    }
+}
+
+  return (
+    <>
+    <h1>{inputValue}</h1>
+    <form onSubmit={ handleSubtim }>
+        <input
+            type="text"
+            value= { inputValue }
+            onChange={ handleInputChange }
+        />
+    </form>
+    </>
+  )
+}
+
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired
+}
