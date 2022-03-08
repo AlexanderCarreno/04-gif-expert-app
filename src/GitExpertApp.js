ddcf48064ae411267883bdd78af99a0554b8a795
Alexander <47396008+AlexanderCarreno@users.noreply.github.com>
2022-03-07 19:54:15 -0600

    GitExpertApp refactoring

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -1,33 +1,36 @@
 //rafc
 import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 const GitExpertApp = ({}) => {
-    //const  categories = ['One Punch', 'Guerra de magos', 'Demon Slayer'];
+    const [categories, setCategories] = useState(['My Hero Academia']);
 
-    const [categories, setCategories] = useState(['One Punch', 'Guerra de magos', 'Demon Slayer']);
-
-    const handleAdd = () => {
+    /*const handleAdd = () => {
         //const newElement = ['My Hero Academia'];
         //setCategories([...categories, ...newElement]);
 
         //setCategories([...categories, 'My Hero Academia']);
 
         setCategories( cats => [...cats, 'My Hero Academia'])
-    }
+    }*/
 
 
     return(
         <>
             <h2>GitExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
             <hr/>
 
-            <button onClick={ handleAdd }>Agregar</button>
 
             <ol>
                 { 
-                    categories.map( category =>{
-                        return <li key={ category } >{category}</li>
-                    })
+                    categories.map( category => ( 
+                        <GifGrid 
+                            key={ category }
+                            category={ category }
+                        />
+                    ))
                 }
             </ol>
         </>
