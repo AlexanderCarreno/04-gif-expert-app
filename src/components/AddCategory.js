56bddebb92c45501baeba143675e3e384a1a3262
Alexander <47396008+AlexanderCarreno@users.noreply.github.com>
2022-03-12 17:51:15 -0600

    unit test for AddCategory component

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -9,12 +9,15 @@ export const AddCategory = ({
 const [inputValue, setInputValue] = useState('');
 
 const handleInputChange = (e)=> {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     setInputValue( e.target.value );
+
+    //console.log('handleInputChanges llamado');
 }
 
 const handleSubtim = (e) => {
     e.preventDefault();
+    //console.log('handleSubmit', inputValue);
 
     if( inputValue.trim().length > 2 ){
       setCategories( cats => [inputValue, ...cats]);
@@ -27,6 +30,7 @@ const handleSubtim = (e) => {
     <>
     <h1>{inputValue}</h1>
     <form onSubmit={ handleSubtim }>
+      <p>{ inputValue }</p>
         <input
             type="text"
             value= { inputValue }
